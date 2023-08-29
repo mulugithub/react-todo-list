601f7bce17e2f1b6b2583d0338237ae5996a877e
Muluken Zemed <zmmulu@gmail.com>
2023-08-29 20:56:43 +0300

    Implement useState hook to toggle drop down menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>
-        <button>
-          Services
-          {' '}
-          <span>&#8595;</span>
-        </button>
-        <ul>
-          <li>Design</li>
-          <li>Development</li>
-        </ul>
-      </li>
-    </ul>
-  </nav>
-);
+import './styles.css';
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>
+          <button onClick={() => setDropdown((prev) => !prev)}>
+            Services
+            {' '}
+            <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
