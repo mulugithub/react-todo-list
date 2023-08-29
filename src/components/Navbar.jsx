7a0001eeca98b0be110a3f5e73b5c76f9bf90303
Muluken Zemed <zmmulu@gmail.com>
2023-08-29 21:03:37 +0300

    Access DOM elements using useRef hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,21 @@
-import './styles.css';
-import { useState } from 'react';
-
+import { useState, useRef } from "react";
 const Navbar = () => {
   const [dropdown, setDropdown] = useState(false);
-
+  
+  const ref = useRef();
+  console.log(ref);
+  
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>
-          <button onClick={() => setDropdown((prev) => !prev)}>
-            Services
-            {' '}
-            <span>&#8595;</span>
-          </button>
-          {dropdown && (
-            <ul>
-              <li>Design</li>
-              <li>Development</li>
-            </ul>
-          )}
-        </li>
-      </ul>
-    </nav>
-  );
-};
-export default Navbar;
+    // ...
+    <li ref={ref}>
+      <button onClick={() => setDropdown((prev) => !prev)}>
+        Services <span>&#8595;</span>
+      </button>
+      {dropdown && (
+        <ul>
+          <li>Design</li>
+          <li>Development</li>
+        </ul>
+      )}
+    </li>
+
