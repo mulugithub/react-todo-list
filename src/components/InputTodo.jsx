9f4be984c9df14a0c668bc440706dcfb13e01a2c
Muluken Zemed <zmmulu@gmail.com>
2023-08-30 03:55:03 +0300

    Create InputTodo component

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,2 +1,22 @@
-const InputTodo = () => <div>input field here...</div>;
+import { useState } from 'react';
+
+const InputTodo = () => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Add Todo..."
+        value={title}
+        onChange={handleChange}
+      />
+      <button type="button">Submit</button>
+    </form>
+  );
+};
 export default InputTodo;
